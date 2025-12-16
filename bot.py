f3fd92bd25cfc6ad75b597daefbe97f98c9723c0
Januth Nimnal <96273863+Januth1234@users.noreply.github.com>
2025-12-16 10:10:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "5669464149:AAEqXH8reDFqnJnYVw3tlCxQHfaTFloDD0k"
+BOT_TOKEN = os.getenv("5669464149:AAEqXH8reDFqnJnYVw3tlCxQHfaTFloDD0k")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is live. What’s the ask?")
+    await update.message.reply_text("Bot is live.")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
